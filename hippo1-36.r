0738547c04b60657563631007df8f64757ff6f25
ziahasanz <103136319+ziahasanz@users.noreply.github.com>
2023-06-26 19:57:37 -0230

    Update hippo1-36.r

--- hippo1-36.r
+++ hippo1-36.r
@@ -1,116 +1,156 @@
 #26.6.23 hippo samples analysis
+
 library(Seurat)
 library(dplyr)
 library(Matrix)
 library(ggpubr)
-#load the >>>>>>>>dataset_1h
-data1 <- Read10X("C:/Users/ziaha/OneDrive/Documents/MobaXterm/home/1H_filtered_feature_bc_matrix")
-H1 <- CreateSeuratObject(counts = data1,project = 'hippo', min.cells =3, min.features=200)
+#load the >>>>>>>>dataset_13h
+data13 <- Read10X("C:/Users/ziaha/OneDrive/Documents/MobaXterm/home/13H_filtered_feature_bc_matrix")
+H13 <- CreateSeuratObject(counts = data13,project = 'hippo', min.cells =3, min.features=200)
 
-#load the >>>>>>>>dataset_2h
-data2 <- Read10X("C:/Users/ziaha/OneDrive/Documents/MobaXterm/home/2H_filtered_feature_bc_matrix")
-H2 <- CreateSeuratObject(counts = data2,project = 'hippo', min.cells =3, min.features=200)
+#load the >>>>>>>>dataset_14h
+data14 <- Read10X("C:/Users/ziaha/OneDrive/Documents/MobaXterm/home/14H_filtered_feature_bc_matrix")
+H14 <- CreateSeuratObject(counts = data14,project = 'hippo', min.cells =3, min.features=200)
 
-#load the >>>>>>>>dataset_3h
-data3 <- Read10X("C:/Users/ziaha/OneDrive/Documents/MobaXterm/home/3H_filtered_feature_bc_matrix")
-H3 <- CreateSeuratObject(counts = data3,project = 'hippo', min.cells =3, min.features=200)
+#load the >>>>>>>>dataset_15h
+data15 <- Read10X("C:/Users/ziaha/OneDrive/Documents/MobaXterm/home/15H_filtered_feature_bc_matrix")
+H15 <- CreateSeuratObject(counts = data15,project = 'hippo', min.cells =3, min.features=200)
+
+#-----------------------------------------------------------------
+#load the >>>>>>>>dataset_19h
+data19 <- Read10X("C:/Users/ziaha/OneDrive/Documents/MobaXterm/home/19H_filtered_feature_bc_matrix")
+H19 <- CreateSeuratObject(counts = data15,project = 'hippo', min.cells =3, min.features=200)
+
+#load the >>>>>>>>dataset_20h
+data20 <- Read10X("C:/Users/ziaha/OneDrive/Documents/MobaXterm/home/20H_filtered_feature_bc_matrix")
+H20 <- CreateSeuratObject(counts = data15,project = 'hippo', min.cells =3, min.features=200)
+
+#load the >>>>>>>>dataset_21h
+data21 <- Read10X("C:/Users/ziaha/OneDrive/Documents/MobaXterm/home/21H_filtered_feature_bc_matrix")
+H21 <- CreateSeuratObject(counts = data15,project = 'hippo', min.cells =3, min.features=200)
 
 
 # QC
 # % MT reads
-H1[["percent.mt"]] <- PercentageFeatureSet(H1, pattern = "^Mt-")
-hp1 <- VlnPlot(H1, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
-
-plot1 <- FeatureScatter(H1, feature1 = "nCount_RNA", feature2 = "percent.mt")
-plot2 <- FeatureScatter(H1, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+H13[["percent.mt"]] <- PercentageFeatureSet(H13, pattern = "^Mt-")
+hp13 <- VlnPlot(H13, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+hp13
+plot1 <- FeatureScatter(H13, feature1 = "nCount_RNA", feature2 = "percent.mt")
+plot2 <- FeatureScatter(H13, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
 CombinePlots(plots = list(plot1, plot2))
 
 
-H2[["percent.mt"]] <- PercentageFeatureSet(H2, pattern = "^Mt-")
-hp2 <- VlnPlot(H2, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
-
-plot3 <- FeatureScatter(H2, feature1 = "nCount_RNA", feature2 = "percent.mt")
-plot4 <- FeatureScatter(H2, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+H14[["percent.mt"]] <- PercentageFeatureSet(H14, pattern = "^Mt-")
+hp14 <- VlnPlot(H14, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+hp14
+plot3 <- FeatureScatter(H14, feature1 = "nCount_RNA", feature2 = "percent.mt")
+plot4 <- FeatureScatter(H14, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
 CombinePlots(plots = list(plot3, plot4))
 
-H3[["percent.mt"]] <- PercentageFeatureSet(H3, pattern = "^Mt-")
-hp3 <- VlnPlot(H3, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
-hp3
+H15[["percent.mt"]] <- PercentageFeatureSet(H15, pattern = "^Mt-")
+hp15 <- VlnPlot(H15, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+hp15
 
-plot5 <- FeatureScatter(H3, feature1 = "nCount_RNA", feature2 = "percent.mt")
-plot6 <- FeatureScatter(H3, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+plot5 <- FeatureScatter(H15, feature1 = "nCount_RNA", feature2 = "percent.mt")
+plot6 <- FeatureScatter(H15, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
 CombinePlots(plots = list(plot5, plot6))
+
+H19[["percent.mt"]] <- PercentageFeatureSet(H19, pattern = "^Mt-")
+hp19 <- VlnPlot(H19, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+hp19
+plot7 <- FeatureScatter(H19, feature1 = "nCount_RNA", feature2 = "percent.mt")
+plot8 <- FeatureScatter(H19, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+CombinePlots(plots = list(plot7, plot8))
+
+H20[["percent.mt"]] <- PercentageFeatureSet(H20, pattern = "^Mt-")
+hp20 <- VlnPlot(H20, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+hp20
+plot9 <- FeatureScatter(H13, feature1 = "nCount_RNA", feature2 = "percent.mt")
+plot10 <- FeatureScatter(H13, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+CombinePlots(plots = list(plot9, plot10))
+
+H21[["percent.mt"]] <- PercentageFeatureSet(H21, pattern = "^Mt-")
+hp21 <- VlnPlot(H21, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+hp21
+plot11 <- FeatureScatter(H13, feature1 = "nCount_RNA", feature2 = "percent.mt")
+plot12 <- FeatureScatter(H13, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+CombinePlots(plots = list(plot11, plot12))
 
 hp1+hp2+hp3
 
 #Filtering
-H1f <- subset(H1, subset = nFeature_RNA > 200 & nFeature_RNA < 7500 & percent.mt < 5)
-H2f <- subset(H2, subset = nFeature_RNA > 200 & nFeature_RNA < 7500 & percent.mt < 5)
-H3f <- subset(H3, subset = nFeature_RNA > 200 & nFeature_RNA < 8000 & percent.mt < 5)
+H13f <- subset(H13, subset = nFeature_RNA > 200 & nFeature_RNA < 7500 & percent.mt < 5)
+H14f <- subset(H14, subset = nFeature_RNA > 200 & nFeature_RNA < 6000 & percent.mt < 5)
+H15f <- subset(H15, subset = nFeature_RNA > 200 & nFeature_RNA < 7500 & percent.mt < 5)
+
+#--------------------------------------------------------------------------------------------
+H19f <- subset(H19, subset = nFeature_RNA > 200 & nFeature_RNA < 7500 & percent.mt < 5)
+H20f <- subset(H20, subset = nFeature_RNA > 200 & nFeature_RNA < 6000 & percent.mt < 5)
+H21f <- subset(H21, subset = nFeature_RNA > 200 & nFeature_RNA < 7500 & percent.mt < 5)
 
 #merged the surat object
-merged.H123 <- merge(H1f, y = c(H2f,H3f), add.cell.ids = c("1H", "2H", "3H"), project = "hippo123")
+merged.H131415 <- merge(H13f, y = c(H14f,H15f), add.cell.ids = c("13H", "14H", "15H"), project = "hippo131415")
 
-# head(colnames(merged.H123))
-# tail(colnames(merged.H123))
-# unique(sapply(X = strsplit(colnames(merged.H123), split = "_"), FUN = "[", 1))
-# table(merged.H123$orig.ident)
+# head(colnames(merged.H131415))
+# tail(colnames(merged.H131415))
+# unique(sapply(X = strsplit(colnames(merged.H131415), split = "_"), FUN = "[", 1))
+# table(merged.H131415$orig.ident)
 
 #adding meta data
 library(stringr)
-sample <- names(merged.H123@active.ident)
-sample_detect <- ifelse(str_detect(sample, "1H"), "1H", ifelse(str_detect(sample, "2H"), "2H", "3H"))
+sample <- names(merged.H131415@active.ident)
+sample_detect <- ifelse(str_detect(sample, "13H"), "13H", ifelse(str_detect(sample, "14H"), "14H", "15H"))
 
-merged.H123@meta.data$sample <- sample_detect
+merged.H131415@meta.data$sample <- sample_detect
 
-Idents(object = merged.H123) <- "sample"
+Idents(object = merged.H131415) <- "sample"
 
 #Normalized Data and Intergrate the three dataset
-mergedH123.list <-  SplitObject(merged.H123, split.by = "sample")
+merged.H131415.list <-  SplitObject(merged.H131415, split.by = "sample")
 
 # perform standard preprocessing on each object
-for (i in 1:length(mergedH123.list)) {
-  mergedH123.list[[i]] <- NormalizeData(mergedH123.list[[i]], verbose = FALSE)
-  mergedH123.list[[i]] <- FindVariableFeatures(
-    mergedH123.list[[i]], selection.method = "vst",
+for (i in 1:length(merged.H131415.list)) {
+  merged.H131415.list[[i]] <- NormalizeData(merged.H131415.list[[i]], verbose = FALSE)
+  merged.H131415.list[[i]] <- FindVariableFeatures(
+    merged.H131415.list[[i]], selection.method = "vst",
     nfeatures = 2000, verbose = FALSE
   )
 }
 
 # select features that are repeatedly variable across datasets for integration run PCA on each
 # dataset using these features
-features <- SelectIntegrationFeatures(object.list = mergedH123.list)
-mergedH123.list <- lapply(X = mergedH123.list, FUN = function(x) {
+features <- SelectIntegrationFeatures(object.list = merged.H131415.list)
+merged.H131415.list <- lapply(X = merged.H131415.list, FUN = function(x) {
   x <- ScaleData(x, features = features, verbose = FALSE)
   x <- RunPCA(x, features = features, verbose = FALSE)
 })
 
 #Integrate Data
 # find anchors
-anchors <- FindIntegrationAnchors(object.list = mergedH123.list)
+anchors <- FindIntegrationAnchors(object.list = merged.H131415.list)
 # integrate data
-mergedH123.list <- IntegrateData(anchorset = anchors)
+merged.H131415.list <- IntegrateData(anchorset = anchors)
 
 #Data Cleaning and Dimensional reduction for visualization
-mergedH123.list <- ScaleData(mergedH123.list, verbose = FALSE)
-mergedH123.list <- FindVariableFeatures(mergedH123.list, 
+merged.H131415.list <- ScaleData(merged.H131415.list, verbose = FALSE)
+merged.H131415.list <- FindVariableFeatures(merged.H131415.list, 
                                        selection.method = "vst",
                                        nfeatures = 2000, 
                                        verbose = FALSE)
 
-mergedH123.list <- RunPCA(mergedH123.list, npcs = 30, verbose = FALSE)
-mergedH123.list <- RunUMAP(mergedH123.list, reduction = "pca", dims = 1:30)
-mergedH123.list <- FindNeighbors(mergedH123.list, reduction = "pca", dims = 1:30)
-mergedH123.list <- FindClusters(mergedH123.list, resolution = 0.5)
-
+merged.H131415.list <- RunPCA(merged.H131415.list, npcs = 30, verbose = FALSE)
+merged.H131415.list <- RunUMAP(merged.H131415.list, reduction = "pca", dims = 1:30)
+merged.H131415.list <- FindNeighbors(merged.H131415.list, reduction = "pca", dims = 1:30)
+merged.H131415.list <- FindClusters(merged.H131415.list, resolution = 0.5)
+gc()
 # Visualization
 
-p1 <- DimPlot(mergedH123.list, reduction = "umap",label = TRUE)
+p1 <- DimPlot(merged.H131415.list, reduction = "umap",label = TRUE)
 p1
-Idents(object = mergedH123.list) <- "sample"
-p2 <- DimPlot(mergedH123.list, reduction = "umap", cols = c("#cc0000", "#38761d","#9fc5e8"))
+Idents(object = merged.H131415.list) <- "sample"
+p2 <- DimPlot(merged.H131415.list, reduction = "umap", cols = c("#cc0000", "#38761d","#9fc5e8"))
 p2
 p1+p2
 
 #To visualize the two conditions side-by-side, we can use the split.by argument to show each condition colored by cluster.
-DimPlot(mergedH123.list, reduction = "umap", split.by = "sample")
+DimPlot(merged.H131415.list, reduction = "umap", split.by = "sample")
